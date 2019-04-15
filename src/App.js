7e5c325a6445bff94fa694bb36c3cc56e23da799
mymrtt <mymrtt@gmail.com>
2019-04-15 12:13:42 -0300

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Footer from './components/Footer';
+
 import './App.css';
 
 class App extends Component {
@@ -6,7 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Bem na Web!</h1>
-        <h1>Bem na Web!</h1>
+        <Footer />
       </div>
     );
   }
